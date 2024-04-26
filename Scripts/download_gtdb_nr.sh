4117ea5f8de7355a0336481b9714d1b049f25df2
Calum Walsh <cazzlewazzle89@gmail.com>
2024-04-26 10:34:31 +1000

    added download scripts

---
+++ Scripts/download_gtdb_nr.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl https://zenodo.org/records/10889037/files/gtdb207nr_nrCon.fna.gz -o gtdb207nr_nrCon.fna.gz
+curl https://zenodo.org/records/10889037/files/gtdb207nr_nrRep.fna.gz -o gtdb207nr_nrRep.fna.gz
+curl https://zenodo.org/records/10889037/files/gtdb207nr_taxLCA.tsv -o gtdb207nr_taxLCA.tsv 
+curl https://zenodo.org/records/10889037/files/gtdb207nr_taxMaj.tsv -o gtdb207nr_taxMaj.tsv
+curl https://zenodo.org/records/10889037/files/gtdb207nr_taxRep.tsv -o gtdb207nr_taxRep.tsv 
---
+++ Scripts/download_refseq_full.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+curl https://zenodo.org/records/10889037/files/refseq207full_full.fna.gz -o refseq207full_full.fna.gz
+curl https://zenodo.org/records/10889037/files/refseq207full_taxFull.tsv -o refseq207full_taxFull.tsv
